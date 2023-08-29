68f968c1af64f1e40a5a77dad9d7e374c8d37abc
shub-171 <shubhamagrawal14497@gmail.com>
2023-08-29 22:54:28 +0530

    first

---
+++ src/app/Components/Product.js
@@ -0,0 +1,60 @@
+"use client"
+import React, { useState, useRef, useEffect } from 'react';
+import products from "../Components/Products.json";
+
+const Product = () => {
+    const [searchQuery, setSearchQuery] = useState('');
+    const searchInputRef = useRef(null);
+
+    // Function to filter products based on search query
+    const filteredProducts = products.filter(product =>
+        product.title.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+
+    const handleSearchChange = (event) => {
+        setSearchQuery(event.target.value);
+    };
+
+    const handleKeyDown = (event) => {
+        if (event.key === '/') {
+            event.preventDefault(); 
+            searchInputRef.current.focus(); 
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('keydown', handleKeyDown);
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown);
+        };
+    }, []);
+
+    return (
+        <>
+        <div className="search">
+            <input
+                type="text"
+                id="searchInput"
+                placeholder="Type / to Search..."
+                value={searchQuery}
+                onChange={handleSearchChange}
+                ref={searchInputRef}
+            />
+            </div>
+            <div className="container">
+                {
+                    filteredProducts.map((product) => (
+                        <a href="https://h" target="_blank" rel="noreferrer" key={product.url}>
+                            <div className="product">
+                                <img className='product_image' src={product.productImage} alt={product.title} title={product.title} />
+                                <span className="product_title">{product.title}</span>
+                            </div>
+                        </a>
+                    ))
+                }
+            </div>
+            </>
+    );
+}
+
+export default Product;
