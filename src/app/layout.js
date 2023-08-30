c8ddeb948ab0ba5b22cf4b260696053103c07a27
shub-171 <shubhamagrawal14497@gmail.com>
2023-08-30 18:31:08 +0530

    og-image

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
 
         <meta property="og:title" content="Give Discount - Discounted Amazon Products, Deals, and Savings" />
         <meta property="og:description" content="Find a wide selection of discounted Amazon products on Give Discount. Explore great deals on electronics, fashion, home goods, and more. Click to buy and save money while shopping through our affiliate links!" />
-        <meta property="og:image" content="../Images/" />
+        <meta property="og:image" content="../Images/Give Discount.png" />
         <meta property="og:url" content="https://givediscount.netlify.app/" />
 
         <meta name="twitter:card" content="summary_large_image" />
