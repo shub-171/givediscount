f5123ffbcb0b76f7b6a18066081c9c74baa91962
shub-171 <shubhamagrawal14497@gmail.com>
2023-09-01 00:02:05 +0530

    s

--- src/app/Components/Product.js
+++ src/app/Components/Product.js
@@ -1,23 +1,13 @@
-"use client"
 import React from 'react';
 import products from "./Products.json";
 
-const shuffleArray = (array) => {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-};
-
 const Product = () => {
-    const shuffledProducts = shuffleArray([...products]);
 
     return (
         <>
             <div className="container">
                 {
-                    shuffledProducts.map((product) => (
+                    products.map((product) => (
                         <a href={product.url} target="_blank" rel="noreferrer" key={product.url}>
                             <div className="product">
                                 <img className='product_image' src={`../Images/products/${product.title}.jpg`} alt={product.title} title={product.title} />
