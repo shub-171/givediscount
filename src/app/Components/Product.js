e3326d29222e0fb20889dc2c95df193c977075c6
shub-171 <shubhamagrawal14497@gmail.com>
2023-08-31 23:40:17 +0530

    31-8-2

--- src/app/Components/Product.js
+++ src/app/Components/Product.js
@@ -1,59 +1,32 @@
-"use client"
-import React, { useState, useRef, useEffect } from 'react';
+import React from 'react';
 import products from "./Products.json";
 
+const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+};
+
 const Product = () => {
-    const [searchQuery, setSearchQuery] = useState('');
-    const searchInputRef = useRef(null);
-
-    // Function to filter products based on search query
-    const filteredProducts = products.filter(product =>
-        product.title.toLowerCase().includes(searchQuery.toLowerCase())
-    );
-
-    const handleSearchChange = (event) => {
-        setSearchQuery(event.target.value);
-    };
-
-    const handleKeyDown = (event) => {
-        if (event.key === '/') {
-            event.preventDefault(); 
-            searchInputRef.current.focus(); 
-        }
-    };
-
-    useEffect(() => {
-        document.addEventListener('keydown', handleKeyDown);
-        return () => {
-            document.removeEventListener('keydown', handleKeyDown);
-        };
-    }, []);
+    const shuffledProducts = shuffleArray([...products]);
 
     return (
         <>
-        <div className="search">
-            <input
-                type="text"
-                id="searchInput"
-                placeholder="Type / to Search..."
-                value={searchQuery}
-                onChange={handleSearchChange}
-                ref={searchInputRef}
-            />
-            </div>
             <div className="container">
                 {
-                    filteredProducts.map((product) => (
+                    shuffledProducts.map((product) => (
                         <a href={product.url} target="_blank" rel="noreferrer" key={product.url}>
                             <div className="product">
-                                <img className='product_image' src={product.productImage} alt={product.title} title={product.title} />
+                                <img className='product_image' src={`../Images/products/${product.title}.jpg`} alt={product.title} title={product.title} />
                                 <span className="product_title">{product.title}</span>
                             </div>
                         </a>
                     ))
                 }
             </div>
-            </>
+        </>
     );
 }
 
