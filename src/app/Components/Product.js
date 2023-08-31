6165700cce61406bf4d11aca04208f1307dcc331
shub-171 <shubhamagrawal14497@gmail.com>
2023-09-01 00:03:48 +0530

    s

--- src/app/Components/Product.js
+++ src/app/Components/Product.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import products from "./Products.json";
 
+const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+};
+
 const Product = () => {
+    const [shuffledProducts, setShuffledProducts] = useState([]);
+
+    useEffect(() => {
+        const storedShuffledProducts = localStorage.getItem('shuffledProducts');
+
+        if (storedShuffledProducts) {
+            setShuffledProducts(JSON.parse(storedShuffledProducts));
+        } else {
+            const newShuffledProducts = shuffleArray([...products]);
+            setShuffledProducts(newShuffledProducts);
+            localStorage.setItem('shuffledProducts', JSON.stringify(newShuffledProducts));
+        }
+    }, []);
 
     return (
         <>
             <div className="container">
                 {
-                    products.map((product) => (
+                    shuffledProducts.map((product) => (
                         <a href={product.url} target="_blank" rel="noreferrer" key={product.url}>
                             <div className="product">
                                 <img className='product_image' src={`../Images/products/${product.title}.jpg`} alt={product.title} title={product.title} />
