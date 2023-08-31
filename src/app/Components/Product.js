6281d242a156bf02391e5325df661bc3f0ca7fc0
Shubham Agrawal <96643129+shub-171@users.noreply.github.com>
2023-08-31 16:16:28 +0530

    Update Product.js

--- src/app/Components/Product.js
+++ src/app/Components/Product.js
@@ -1,6 +1,6 @@
 "use client"
 import React, { useState, useRef, useEffect } from 'react';
-import products from "../Components/Products.json";
+import products from "./Products.json";
 
 const Product = () => {
     const [searchQuery, setSearchQuery] = useState('');
