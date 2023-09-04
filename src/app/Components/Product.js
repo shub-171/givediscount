38b5c9e23b8205b61841702758e53a218e0ee2ae
shub-171 <shubhamagrawal14497@gmail.com>
2023-09-04 21:09:10 +0530

    s

--- src/app/Components/Product.js
+++ src/app/Components/Product.js
@@ -1,43 +1,34 @@
 "use client"
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import products from "./Products.json";
 
-const shuffleArray = (array) => {
-    for (let i = array.length - 1; i > 0; i--) {
+function shuffleArray(array) {
+    let newArray = [...array];
+    for (let i = newArray.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
+        [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
     }
-    return array;
-};
+    return newArray;
+}
 
-const Product = () => {
+function Product() {
     const [shuffledProducts, setShuffledProducts] = useState([]);
 
     useEffect(() => {
-        const storedShuffledProducts = localStorage.getItem('shuffledProducts');
-
-        if (storedShuffledProducts) {
-            setShuffledProducts(JSON.parse(storedShuffledProducts));
-        } else {
-            const newShuffledProducts = shuffleArray([...products]);
-            setShuffledProducts(newShuffledProducts);
-            localStorage.setItem('shuffledProducts', JSON.stringify(newShuffledProducts));
-        }
+        setShuffledProducts(shuffleArray(products));
     }, []);
 
     return (
         <>
             <div className="container">
-                {
-                    shuffledProducts.map((product) => (
-                        <a href={product.url} target="_blank" rel="noreferrer" key={product.url}>
-                            <div className="product">
-                                <img className='product_image' src={`../Images/products/${product.title}.jpg`} alt={product.title} title={product.title} />
-                                <span className="product_title">{product.title}</span>
-                            </div>
-                        </a>
-                    ))
-                }
+                {shuffledProducts.map((product) => (
+                    <a href={product.url} target="_blank" rel="noreferrer" key={product.url}>
+                        <div className="product">
+                            <img className='product_image' src={`../Images/products/${product.title}.jpg`} alt={product.title} title={product.title} />
+                            <span className="product_title">{product.title}</span>
+                        </div>
+                    </a>
+                ))}
             </div>
         </>
     );
